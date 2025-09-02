74cef1075f36fcd81ff5d39d8f8563bee705226a
tejassingh_1232 <148686413+tejassingh-1232@users.noreply.github.com>
2025-09-02 09:03:18 +0530

    Update DirectMessage.js

--- backend/models/DirectMessage.js
+++ backend/models/DirectMessage.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const directMessageSchema = new mongoose.Schema({
+const DirectMessageSchema = new mongoose.Schema({
   from: String,
   to: String,
   text: String,
   time: String,
-  date: String
+  date: String,
+  reactions: { type: Map, of: [String], default: {} },
+  seen: { type: [String], default: [] }
 });
 
 export default mongoose.model("DirectMessage", directMessageSchema);
